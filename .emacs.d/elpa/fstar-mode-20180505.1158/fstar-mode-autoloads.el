ddf9cc2e0029a95584099bcb95ba2f5235876d43
Nitin <nitinjohnraj@gmail.com>
2018-05-22 15:21:40 +0530

    bashrc and init.el

--- .emacs.d/elpa/fstar-mode-20180505.1158/fstar-mode-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; fstar-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "fstar-mode" "fstar-mode.el" (23280 50100 528526
-;;;;;;  444000))
-;;; Generated autoloads from fstar-mode.el
-
-(put 'fstar-comment-style 'safe-local-variable #'symbolp)
-
-(autoload 'fstar-mode "fstar-mode" "\
-
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.fsti?\\'" . fstar-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("fstar-mode-pkg.el") (23280 50100 550231
-;;;;;;  994000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; fstar-mode-autoloads.el ends here
